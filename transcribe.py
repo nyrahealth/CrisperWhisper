819bd49d23776b7c989affa8108f3e65807fd180
LaurinmyReha <90200746+LaurinmyReha@users.noreply.github.com>
2024-09-02 16:26:58 +0200

    Update transcribe.py

--- transcribe.py
+++ transcribe.py
@@ -10,7 +10,7 @@ def transcribe_audio(file_path):
     torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
 
     model_id = (
-        "openai/whisper-large-v3"  # You can change this to a different model if needed
+        "nyrahealth/CrisperWhisper"  # You can change this to a different model if needed
     )
 
     model = AutoModelForSpeechSeq2Seq.from_pretrained(
